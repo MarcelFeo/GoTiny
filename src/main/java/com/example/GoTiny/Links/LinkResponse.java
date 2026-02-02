ad5afe04c602d0d1eebf7e2522461a721563ebf7
MarcelFeo <marcelfeo29@gmail.com>
2026-02-02 18:34:16 -0300

    add LinkModel

---
+++ src/main/java/com/example/GoTiny/Links/LinkResponse.java
@@ -0,0 +1,65 @@
+package com.example.GoTiny.Links;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class LinkResponse {
+
+    private UUID id;
+    private String urlLong;
+    private String urlShort;
+    private String urlQrCode;
+    private LocalDateTime urlCreated_at;
+
+    public LinkResponse(UUID id, String urlLong, String urlShort, String urlQrCode, LocalDateTime urlCreated_at) {
+        this.id = id;
+        this.urlLong = urlLong;
+        this.urlShort = urlShort;
+        this.urlQrCode = urlQrCode;
+        this.urlCreated_at = urlCreated_at;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUrlLong() {
+        return urlLong;
+    }
+
+    public void setUrlLong(String urlLong) {
+        this.urlLong = urlLong;
+    }
+
+    public String getUrlShort() {
+        return urlShort;
+    }
+
+    public void setUrlShort(String urlShort) {
+        this.urlShort = urlShort;
+    }
+
+    public String getUrlQrCode() {
+        return urlQrCode;
+    }
+
+    public void setUrlQrCode(String urlQrCode) {
+        this.urlQrCode = urlQrCode;
+    }
+
+    public LocalDateTime getUrlCreated_at() {
+        return urlCreated_at;
+    }
+
+    public void setUrlCreated_at(LocalDateTime urlCreated_at) {
+        this.urlCreated_at = urlCreated_at;
+    }
+}
