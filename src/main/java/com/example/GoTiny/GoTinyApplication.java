e4952741acebb56e24597ca3f5a407a9e3c15b96
MarcelFeo <marcelfeo29@gmail.com>
2026-02-02 18:08:58 -0300

    start project

---
+++ src/main/java/com/example/GoTiny/GoTinyApplication.java
@@ -0,0 +1,13 @@
+package com.example.GoTiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoTinyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoTinyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GoTiny/GoTinyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GoTiny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoTinyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
