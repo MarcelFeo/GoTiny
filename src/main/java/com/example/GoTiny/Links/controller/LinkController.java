42d7aa59e272eac20640be2bde80953dd2c3d224
MarcelFeo <marcelfeo29@gmail.com>
2026-02-04 18:31:34 -0300

    add controller

--- src/main/java/com/example/GoTiny/Links/controller/LinkController.java
+++ src/main/java/com/example/GoTiny/Links/controller/LinkController.java
@@ -1,7 +1,43 @@
 package com.example.GoTiny.Links.controller;
 
+import com.example.GoTiny.Links.model.LinkResponse;
+import com.example.GoTiny.Links.model.Link;
+import com.example.GoTiny.Links.service.LinkService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
 public class LinkController {
 
+    private LinkService linkService;
 
+    public LinkController(LinkService linkService) {
+        this.linkService = linkService;
+    }
+
+    @PostMapping("/gotiny")
+    public ResponseEntity<LinkResponse> generateShortUrl(@RequestBody Map<String, String> request) {
+
+        String originalUrl = request.get("originalUrl");
+        Link link = linkService.shortenURL(originalUrl);
+
+        String redirectUserUrl = "http://localhost:8081/r/" + link.getUrlShort();
+
+        LinkResponse response = new LinkResponse(
+                link.getId(),
+                link.getUrlLong(),
+                redirectUserUrl,
+                link.getUrlQrCode(),
+                link.getUrlCreated_at()
+        );
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+    }
 
 }
